d49553df6669944f89fabb55e990e142e8b4f7bf
Aizistral <admin@aizistral.com>
2024-01-01 18:52:22 +0100

    Reduce boilerplate in `LeaderboardEntry`

--- src/main/java/com/aizistral/infmachine/data/LeaderboardEntry.java
+++ src/main/java/com/aizistral/infmachine/data/LeaderboardEntry.java
@@ -1,57 +1,11 @@
 package com.aizistral.infmachine.data;
 
-public class LeaderboardEntry
-{
-    private Long userID;
+import lombok.Value;
+
+@Value
+public class LeaderboardEntry {
+    private long userID;
     private String userName;
-    int messageCount;
-    int rating;
-
-    public LeaderboardEntry(Long userID, String userName, int messageCount, int rating)
-    {
-        setUserID(userID);
-        setUserName(userName);
-        setMessageCount(messageCount);
-        setRating(rating);
-    }
-
-    public Long getUserID()
-    {
-        return userID;
-    }
-
-    public void setUserID(Long userID)
-    {
-        this.userID = userID;
-    }
-
-    public String getUserName()
-    {
-        return userName;
-    }
-
-    public void setUserName(String userName)
-    {
-        this.userName = userName;
-    }
-
-    public int getMessageCount()
-    {
-        return messageCount;
-    }
-
-    public void setMessageCount(int messageCount)
-    {
-        this.messageCount = messageCount;
-    }
-
-    public int getRating()
-    {
-        return rating;
-    }
-
-    public void setRating(int rating)
-    {
-        this.rating = rating;
-    }
+    private int messageCount;
+    private int rating;
 }
